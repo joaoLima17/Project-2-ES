d7f4e63b0e9475e079cdc845b7aafdbafd21daf0
anamfrancisco <113603689+anamfrancisco@users.noreply.github.com>
2022-11-29 18:52:28 +0000

    Só sei que nada sei

--- ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
@@ -7,29 +7,11 @@ import net.sourceforge.ganttproject.task.Task;
 
 public class PriorityProjectManagement {
 
-  private static final int JANUARY = 1;
-  private static final int FEBRUARY = 2;
-  private static final int MARCH = 3;
-  private static final int APRIL = 4;
-  private static final int MAY = 5;
-  private static final int JUNE = 6;
-  private static final int JULY = 7;
-  private static final int AUGUST = 8;
-  private static final int SEPTEMBER = 9;
-  private static final int OCTOBER = 10;
-  private static final int NOVEMBER = 11;
-  private static final int DECEMBER = 12;
-
+  private static final int[] MONTHS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   private GanttCalendar earlyStart, lateStart, earlyFinish, lateFinish;
   private float duration, slack;
-
   private Task task;
-
   private boolean isOver;
-
-  private int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-
 
 
   public PriorityProjectManagement(Task task){
@@ -45,8 +27,6 @@ public class PriorityProjectManagement {
       isOver = false;
     }
 
-
-
   }
 
   public void setLateFinish(GanttCalendar lateFinish){
@@ -55,22 +35,14 @@ public class PriorityProjectManagement {
 
   public void calculateSlack() {
     //slack = lateStart - earlyStart
-    int lateStartMonth, earlyStartMonth;
-    if(lateStart.getMonth() == 2){
-
-    }
     int totalDays = 0;
     for(int i = 0; i < 12; i++){
-      if(lateStart.getMonth() == i){
-
+      while(i > lateStart.getMonth() && i < earlyStart.getMonth()){
+        totalDays += MONTHS[i];
       }
     }
 
-
-
-    slack = ((lateStart.getYear() - earlyStart.getYear()) * 365) + ((lateStart.getMonth() - earlyStart.getMonth()) * 30) + (lateStart.getDay() - earlyStart.getDay());
-
-
+    slack = ((lateStart.getYear() - earlyStart.getYear()) * 365) + totalDays + (lateStart.getDay() - earlyStart.getDay());
   }
 
   public void calculateLateStart() { //lateFinish - duration;
