3b3a519506f5d37c6490619a0ddbb24fdd6ad8bf
anamfrancisco <113603689+anamfrancisco@users.noreply.github.com>
2022-11-29 18:34:14 +0000

    Alterações no código

--- ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
@@ -2,33 +2,111 @@ package net.sourceforge.ganttproject;
 
 import biz.ganttproject.core.time.GanttCalendar;
 import biz.ganttproject.core.time.TimeDuration;
+import net.sourceforge.ganttproject.task.Task;
+
 
 public class PriorityProjectManagement {
 
-  GanttCalendar earlyStart, lateStart, earlyFinish, lateFinish;
-  TimeDuration duration, slack;
+  private static final int JANUARY = 1;
+  private static final int FEBRUARY = 2;
+  private static final int MARCH = 3;
+  private static final int APRIL = 4;
+  private static final int MAY = 5;
+  private static final int JUNE = 6;
+  private static final int JULY = 7;
+  private static final int AUGUST = 8;
+  private static final int SEPTEMBER = 9;
+  private static final int OCTOBER = 10;
+  private static final int NOVEMBER = 11;
+  private static final int DECEMBER = 12;
 
-  boolean isOver;
+  private GanttCalendar earlyStart, lateStart, earlyFinish, lateFinish;
+  private float duration, slack;
+
+  private Task task;
+
+  private boolean isOver;
+
+  private int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 
-  public PriorityProjectManagement(GanttCalendar start, GanttCalendar finish, TimeDuration duration){
-    this.duration = duration;
-    this.earlyStart = start;
-    this.earlyFinish = finish;
-    //verificacao se acabou
-  }
 
 
-  public void setLateStart(GanttCalendar lateStart){
-    this.lateStart = lateStart;
+  public PriorityProjectManagement(Task task){
+    this.task = task;
+    this.duration = task.getDuration().getValue();
+    this.earlyStart = task.getStart();
+    this.earlyFinish = task.getEnd();
+
+    //isto tem de ser chamado antes de o objeto ser criado
+    if(task.getCompletionPercentage() == 100){
+      isOver = true;
+    } else {
+      isOver = false;
+    }
+
+
+
   }
 
   public void setLateFinish(GanttCalendar lateFinish){
     this.lateFinish = lateFinish;
   }
 
-  public void setSlack(TimeDuration slack) {
-    this.slack = slack;
+  public void calculateSlack() {
+    //slack = lateStart - earlyStart
+    int lateStartMonth, earlyStartMonth;
+    if(lateStart.getMonth() == 2){
+
+    }
+    int totalDays = 0;
+    for(int i = 0; i < 12; i++){
+      if(lateStart.getMonth() == i){
+
+      }
+    }
+
+
+
+    slack = ((lateStart.getYear() - earlyStart.getYear()) * 365) + ((lateStart.getMonth() - earlyStart.getMonth()) * 30) + (lateStart.getDay() - earlyStart.getDay());
+
+
+  }
+
+  public void calculateLateStart() { //lateFinish - duration;
+
+    if(lateFinish.getDay() - duration >= 1){
+      //lateStart = new GanttCalendar(lateFinish.getDay()-duration, lateFinish.getMonth(), lateFinish.getYear(), local);
+    } else {
+      float day;
+      if(lateFinish.getMonth()-1 == 2){
+        if(lateFinish.getYear() % 4 == 0){
+          day = 29 - (lateFinish.getDay() - duration);
+        } else {
+          day = 28 - (lateFinish.getDay() - duration);
+        }
+      } else if (lateFinish.getMonth()-1 == 4 || lateFinish.getMonth()-1 == 6 || lateFinish.getMonth()-1 == 9 ||
+        lateFinish.getMonth()-1 == 11){
+        day = 30 - (lateFinish.getDay() - duration);
+      } else {
+        day = 31 - (lateFinish.getDay() - duration);
+      }
+      //lateStart = new GanttCalendar(day, lateFinish.getMonth()-1, lateFinish.getYear(), local);
+    }
+
+  }
+
+
+  public float getDuration() {
+    return duration;
+  }
+
+  public float getSlack() {
+    return slack;
+  }
+
+  public Task getTask(){
+    return task;
   }
 
   public GanttCalendar getEarlyFinish() {
@@ -39,20 +117,12 @@ public class PriorityProjectManagement {
     return earlyStart;
   }
 
-  public GanttCalendar getLateFinish() {
-    return lateFinish;
-  }
-
-  public GanttCalendar getLateStart() {
+  public GanttCalendar getLateStart(){
     return lateStart;
   }
 
-  public TimeDuration getDuration() {
-    return duration;
-  }
-
-  public TimeDuration getSlack() {
-    return slack;
+  public GanttCalendar getLateFinish() {
+    return lateFinish; //LST(i+1)
   }
 
 }
--- ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityToBeAssignedTask.java
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityToBeAssignedTask.java
@@ -11,25 +11,42 @@ public class PriorityToBeAssignedTask {
 
   //lists
   private final List<Task> allTasks = new ArrayList();
+  private final List<PriorityProjectManagement> allTasksDetailed = new ArrayList();
+
+  private final List<PriorityProjectManagement> allTasksInOrder = new ArrayList();
 
   public PriorityToBeAssignedTask(){
     //atualiza lista de tasks
     //calcula prioridade
+    tasksToDetailedTasks();
     CalculatePriority();
     //organiza nova lista
+  }
+
+  private void tasksToDetailedTasks(){
+    for(int i = 0; i< allTasks.size(); i++){
+      allTasksDetailed.add(new PriorityProjectManagement(allTasks.get(i)));
+    }
   }
 
   public List<Task> getTasksInOrderToBeAssigned() {
     return allTasks;//return lista organizada
   }
 
-  private void CalculatePriority(){
-    for(int i = 0; i < allTasks.size(); i++){
-      if(allTasks.get(i).getStart().after(allTasks.get(i+1).getStart())){
 
+
+  private void CalculatePriority(){
+    /*for(int i = 0; i < allTasks.size(); i++){
+      if(allTasks.get(i).getStart().before(allTasks.get(i).getStart())){
 
 
       }
+    }*/
+
+    for(int i = 0; i < allTasksDetailed.size() ; i++){
+      allTasksDetailed.get(i).setLateFinish(allTasksDetailed.get(i+1).getLateStart()); //LFT(D) = LST(F)
+      allTasksDetailed.get(i).calculateLateStart();//LST(D) = LFT(D) - DURATION(D)
+
     }
 
   }
