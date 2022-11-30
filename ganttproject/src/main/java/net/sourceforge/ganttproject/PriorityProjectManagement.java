2b46a1223d4c2a7e3eb946f69cdc3fc102b861d0
anamfrancisco <113603689+anamfrancisco@users.noreply.github.com>
2022-11-30 11:57:53 +0000

    Correção de algumas funções
    Precisamos de arranjar forma de ordenar a lista eficientemente

--- ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityProjectManagement.java
@@ -2,7 +2,7 @@ package net.sourceforge.ganttproject;
 
 import biz.ganttproject.core.time.CalendarFactory;
 import biz.ganttproject.core.time.GanttCalendar;
-import biz.ganttproject.core.time.TimeDuration;
+
 import net.sourceforge.ganttproject.task.Task;
 
 import java.util.Calendar;
@@ -11,11 +11,9 @@ import java.util.concurrent.TimeUnit;
 
 public class PriorityProjectManagement {
 
-  private static final int[] MONTHS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   private GanttCalendar earlyStart, lateStart, earlyFinish, lateFinish;
   private float duration, slack;
   private Task task;
-  private boolean isOver;
 
 
   public PriorityProjectManagement(Task task){
@@ -23,14 +21,6 @@ public class PriorityProjectManagement {
     this.duration = task.getDuration().getValue();
     this.earlyStart = task.getStart();
     this.earlyFinish = task.getEnd();
-
-    //isto tem de ser chamado antes de o objeto ser criado
-    if(task.getCompletionPercentage() == 100){
-      isOver = true;
-    } else {
-      isOver = false;
-    }
-
   }
 
   public void setLateFinish(GanttCalendar lateFinish){
@@ -39,15 +29,10 @@ public class PriorityProjectManagement {
     calculateLateStart();
   }
 
-  private void calculateSlack() {
-    //slack = lateStart - earlyStart
-
-
-    slack =TimeUnit.DAYS.convert(
-      lateStart.getTime().getTime()-earlyStart.getTime().getTime(), TimeUnit.DAYS);
-
-    }
-
+  private void calculateSlack() { //slack = lateStart - earlyStart
+    slack = TimeUnit.DAYS.convert(
+      lateStart.getTime().getTime() - earlyStart.getTime().getTime(), TimeUnit.DAYS);
+  }
 
   public void calculateLateStart() { //lateFinish - duration;
 
@@ -55,7 +40,7 @@ public class PriorityProjectManagement {
     c.setTime(lateFinish.getTime());
     c.add(Calendar.DAY_OF_MONTH,(int) duration);
 
-    lateStart =CalendarFactory.createGanttCalendar(c.getTime());
+    lateStart = CalendarFactory.createGanttCalendar(c.getTime());
   }
 
 
--- ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityToBeAssignedTask.java
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/PriorityToBeAssignedTask.java
@@ -1,8 +1,6 @@
 package net.sourceforge.ganttproject;
 
 import net.sourceforge.ganttproject.task.Task;
-import net.sourceforge.ganttproject.task.TaskImpl;
-import net.sourceforge.ganttproject.PriorityProjectManagement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +10,6 @@ public class PriorityToBeAssignedTask {
   //lists
   private final List<Task> allTasks = new ArrayList();
   private final List<PriorityProjectManagement> allTasksDetailed = new ArrayList();
-
   private final List<PriorityProjectManagement> allTasksInOrder = new ArrayList();
 
   public PriorityToBeAssignedTask(){
@@ -25,29 +22,40 @@ public class PriorityToBeAssignedTask {
 
   private void tasksToDetailedTasks(){
     for(int i = 0; i< allTasks.size(); i++){
-      allTasksDetailed.add(new PriorityProjectManagement(allTasks.get(i)));
+      if(allTasks.get(i).getCompletionPercentage() < 100) {
+        allTasksDetailed.add(new PriorityProjectManagement(allTasks.get(i)));
+      }
     }
   }
 
   public List<Task> getTasksInOrderToBeAssigned() {
-    return allTasks;//return lista organizada
+    List<Task> finalTasks = new ArrayList<>();
+    for(int i = 0; i < allTasksInOrder.size(); i++){
+      finalTasks.add(allTasksInOrder.get(i).getTask());
+    }
+    return finalTasks;//return lista organizada
   }
 
 
-
   private void CalculatePriority(){
-    /*for(int i = 0; i < allTasks.size(); i++){
-      if(allTasks.get(i).getStart().before(allTasks.get(i).getStart())){
+    //slides
+    for(int i = allTasksDetailed.size() - 1; i >= 0 ; i--){
+      if(i == allTasksDetailed.size()){
+        allTasksDetailed.get(i).setLateFinish(allTasksDetailed.get(i).getEarlyFinish());
+      } else {
+        allTasksDetailed.get(i).setLateFinish(allTasksDetailed.get(i+1).getLateStart()); //LFT(D) = LST(F)
+      }
+    }
 
+    //Temos de arranjar uma maneira de ordenar a lista, podemos fazer um comparador mas nao sei bem fazer isso
+    for(int j = 0; j < allTasksDetailed.size(); j++){
+      if(allTasksDetailed.get(j).getLateFinish().before(allTasksDetailed.get(j+1).getLateFinish())){
+        allTasksInOrder.add(allTasksDetailed.get(j));
+      } else if(allTasksDetailed.get(j).getLateFinish().after(allTasksDetailed.get(j+1).getLateFinish())){
 
       }
-    }*/
-
-    for(int i = 0; i < allTasksDetailed.size() ; i++){
-      allTasksDetailed.get(i).setLateFinish(allTasksDetailed.get(i+1).getLateStart()); //LFT(D) = LST(F)
-      allTasksDetailed.get(i).calculateLateStart();//LST(D) = LFT(D) - DURATION(D)
-
     }
+
 
   }
 
